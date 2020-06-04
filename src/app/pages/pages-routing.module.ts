d0506a34a7ab1e276538cdb233c8c50b622977ca
Greiber Salas <greibersalas@gmsil.com>
2020-06-03 19:22:27 -0500

    Sección 8

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -6,6 +6,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 const routes: Routes = [
@@ -15,15 +17,18 @@ const routes: Routes = [
     children:[
       {
         path:'dashboard',
-        component: DashboardComponent
+        component: DashboardComponent,
+        data:{titulo:'Dashboard'}
       },
       {
         path:'progress',
-        component: ProgressComponent
+        component: ProgressComponent,
+        data:{titulo:'Progress'}
       },
       {
         path:'graficas1',
-        component: Graficas1Component
+        component: Graficas1Component,
+        data:{titulo:'Graficas'}
       },
       {
         path:'',
@@ -32,7 +37,18 @@ const routes: Routes = [
       },
       {
         path:'account-settings',
-        component: AccountSettingsComponent
+        component: AccountSettingsComponent,
+        data:{titulo:'Ajustes del tema'}
+      },
+      {
+        path:'promesas',
+        component: PromesasComponent,
+        data:{titulo:'Promesas'}
+      },
+      {
+        path:'rxjs',
+        component: RxjsComponent,
+        data:{titulo:'RxJs'}
       }
     ]
   },
